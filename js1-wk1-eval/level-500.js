1627db39025caca53ce2edb497f1d017a2f873b2
LauraSantiag0 <eusoulaura8@gmail.com>
2023-09-30 16:04:47 +0100

    500

--- js1-wk1-eval/level-500.js
+++ js1-wk1-eval/level-500.js
@@ -1,7 +1,11 @@
 
 
 const avgDaysInYear = Math.floor(365 / 12);
+console
 
-// Without running any code,
+/*
+/ Without running any code,
 // Predict and explain what the value of avgDaysInYear will evaluate to
 // You can use documentation to look up what Math.floor does
+
+It will show us how many days we have in one month
